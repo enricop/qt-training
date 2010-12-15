786184ae41909aac083d3e16f0650988f49c65d1
Frank Mertens <ext-frank.mertens@nokia.com>
2010-12-15 18:45:20 +0100

    Adding copyright notices and license files.

--- addon/fundamentals/ex-helloworld/main.cpp
+++ addon/fundamentals/ex-helloworld/main.cpp
@@ -1,14 +1,11 @@
-/*
- * main.cpp -- main module
+/*************************************************************************
  *
  * Copyright (c) 2008-2011, Nokia Corporation and/or its subsidiary(-ies).
  * All rights reserved.
  *
  * See the LICENSE.txt file shipped along with this file for the license.
  *
- *
- * This example shows the "Hello, world" in Qt.
- */
+ *************************************************************************/
 
 #include <QtGui>
 
