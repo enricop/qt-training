f54a8ddbd6071f966f287288d268a57f4fe81731
Frank Mertens <ext-frank.mertens@nokia.com>
2011-03-09 15:54:07 +0100

    Adding copyright headers.

--- addon/dataio/mysql_connect.h
+++ addon/dataio/mysql_connect.h
@@ -1,3 +1,12 @@
+/*************************************************************************
+ *
+ * Copyright (c) 2008-2011, Nokia Corporation and/or its subsidiary(-ies).
+ * All rights reserved.
+ *
+ * See the LICENSE.txt file shipped along with this file for the license.
+ *
+ *************************************************************************/
+
 static const char* s_databaseName = "bookstore";
 static const char* s_user = "bookuser";
 static const char* s_password = "bookpass";
--- addon/dataio/sol-bookstore/bookstore.h
+++ addon/dataio/sol-bookstore/bookstore.h
@@ -1,3 +1,12 @@
+/*************************************************************************
+ *
+ * Copyright (c) 2008-2011, Nokia Corporation and/or its subsidiary(-ies).
+ * All rights reserved.
+ *
+ * See the LICENSE.txt file shipped along with this file for the license.
+ *
+ *************************************************************************/
+
 #ifndef BOOKSTORE_H
 #define BOOKSTORE_H
 
