75900c0cae7fbca65736bf6587d27a47829d3dc0
Frank Mertens <ext-frank.mertens@nokia.com>
2011-04-05 11:41:54 +0200

    Adding more info on how to get started.

--- scripts/example_course.py
+++ scripts/example_course.py
@@ -1,7 +1,16 @@
 course = Course()
+
+## edit the following line to point to your clone of the course material
 course.add_source("git", "~/gitorious/course-material")
-course.add_source("svn", "~/qt-training-svn/trunk/course-material")
+
+## optionally add your private repository
+# course.add_source("svn", "~/qt-training-svn/trunk/course-material")
+
+## select training modules to build (subfolders in course-material/slides)
 course.add_module("fundamentals")
 course.add_module("objects")
-course.add_module("widgets", 'svn')
+
+## second argument allows to specify a specific source
+# course.add_module("widgets", 'svn')
+
 course.compile()
