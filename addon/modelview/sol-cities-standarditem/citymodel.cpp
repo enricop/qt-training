08e98271099b77471344188f6f49f777369e5057
Kevin Krammer <kevin.krammer@kdab.com>
2012-06-21 12:28:50 +0200

    Add lab and solution for standard item model

---
+++ addon/modelview/sol-cities-standarditem/citymodel.cpp
@@ -0,0 +1,51 @@
+#include "citymodel.h"
+
+enum ItemRoles {
+    CityRole = Qt::UserRole + 1,
+    CountryRole,
+    PopulationRole,
+    AreaRole,
+    FlagRole
+};
+
+
+CityModel::CityModel(QObject *parent) : QStandardItemModel(parent)
+{
+    setupModel();
+}
+
+void CityModel::setupModel()
+{
+    CityEngine engine;
+
+    const QStringList countries = engine.countries();
+    Q_FOREACH (const QString &country, countries) {
+        const QIcon flag = engine.flag(country);
+        const QStringList cities = engine.citiesByCountry(country);
+
+        Q_FOREACH (const QString &city, cities) {
+            const qreal area = engine.area(city);
+            const int population = engine.population(city);
+
+            QStandardItem *item = new QStandardItem;
+            item->setData(city, CityRole);
+            item->setData(country, CountryRole);
+            item->setData(flag, FlagRole);
+            item->setData(population, PopulationRole);
+            item->setData(area, AreaRole);
+
+            appendRow(item);
+        }
+    }
+
+    QHash<int, QByteArray> roleNames;
+    roleNames[CityRole] = "city";
+    roleNames[CountryRole] = "country";
+    roleNames[FlagRole] = "flag";
+    roleNames[PopulationRole] = "population";
+    roleNames[AreaRole] = "area";
+
+    setRoleNames(roleNames);
+
+    setSortRole(CountryRole);
+}
