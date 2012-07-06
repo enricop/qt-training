af3d9babd9189b494959d08424d698d2ba12d3dd
Jesper K. Pedersen <jesper.pedersen@kdab.com>
2012-07-06 14:33:20 +0200

    ported example

--- addon/object-communication/ex-quotebutton/main.cpp
+++ addon/object-communication/ex-quotebutton/main.cpp
@@ -7,7 +7,7 @@
  *
  *************************************************************************/
 
-#include <QtGui>
+#include <QtWidgets>
 #include "quotebutton.h"
 
 int main(int argc, char *argv[])
