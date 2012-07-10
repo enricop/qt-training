2375a8809b32df838a6efa88436b153f029cd1c5
Kevin Ottens <kevin.ottens@kdab.com>
2012-07-10 08:32:31 +0200

    Make it build in a world with no QApplication

--- addon/coretypes/ex_custom_types/main.cpp
+++ addon/coretypes/ex_custom_types/main.cpp
@@ -1,11 +1,11 @@
-#include <QApplication>
+#include <QCoreApplication>
 #include "Contact.h"
 #include <QVariant>
 #include <QDebug>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QCoreApplication a(argc, argv);
 
     Contact contact;
     contact.setName("Peter");
